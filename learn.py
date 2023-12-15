829e70681aeb1cb601c5c0a69936f05afbff8f81
DrDavidL <92898146+DrDavidL@users.noreply.github.com>
2023-12-14 21:12:00 -0600

    memory working

--- learn.py
+++ learn.py
@@ -1,33 +1,13 @@
-# import json
-# import time
-# import openai
-# import os
+
 import streamlit as st
-# from langchain.utilities import DuckDuckGoSearchAPIWrapper
-# import requests
-# import time
-# from retrying import retry
-from prompts import *
 import os 
-# from io import StringIO, BytesIO
-# from langchain.document_loaders import PyMuPDFLoader
-# from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain.vectorstores import Chroma
 from langchain.embeddings import OpenAIEmbeddings
-# from langchain.chat_models import ChatOpenAI
-# from langchain.chains import RetrievalQA
-# from langchain.chains.question_answering import load_qa_chain
 from langchain.vectorstores import FAISS
-# from langchain.callbacks import get_openai_callback
-# from langchain.llms import OpenAI
 import streamlit as st
-from prompts import *
-# import fitz
-# from io import StringIO      
+from prompts import * 
 from openai import OpenAI
 
 
-disclaimer = """**Disclaimer:** This is a tool to teach students"""
 
 st.set_page_config(page_title='My Tutor', layout = 'centered', page_icon = ':stethoscope:', initial_sidebar_state = 'auto')
 st.title("Learn!")
@@ -38,7 +18,7 @@ with st.expander('Important Disclaimer'):
     st.write("Author: David Liebovitz")
     st.info(disclaimer)
     st.session_state.temp = st.slider("Select temperature (Higher values more creative but tangential and more error prone)", 0.0, 1.0, 0.3, 0.01)
-    st.write("Last updated 10/9/23")
+    st.write("Last updated 12/9/23")
     
 
 def update_messages(messages, system_content=None, assistant_content=None, user_content=None):
@@ -71,23 +51,49 @@ def update_messages(messages, system_content=None, assistant_content=None, user_
 
     return messages
 
-def answer_using_prefix(prefix, sample_question, sample_answer, my_ask, temperature, history_context, model, print = True):
-    # st.write('yes the function is being used!')
-    messages_blank = []
-    messages = update_messages(
-        messages = messages_blank, 
-        system_content=f'{prefix}; Sample question: {sample_question} Sample response: {sample_answer} Preceding conversation: {history_context}', 
-        assistant_content='',
-        user_content=my_ask,
-        )
-    # st.write(messages)
-    # st.write('here is the model: ' + model)
+def gen_response(messages, temperature, model, print = True):
     api_key = st.secrets["OPENAI_API_KEY"]
     client = OpenAI(
         api_key=api_key,
     )
     params = {
-        "model": "gpt-4-1106-preview",
+        "model": model,
+        "messages": messages,
+        "temperature": temperature,
+        "stream": print,
+    }
+    try:    
+        completion = client.chat.completions.create(**params)
+    except Exception as e:
+        st.write(e)
+        st.write(f'Here were the params: {params}')
+        return None
+
+    placeholder = st.empty()
+    full_response = ''
+    for chunk in completion:
+        if chunk.choices[0].delta.content is not None:
+            full_response += chunk.choices[0].delta.content
+            # full_response.append(chunk.choices[0].delta.content)
+            placeholder.markdown(full_response)
+    placeholder.markdown(full_response)
+    return full_response
+
+def answer_using_prefix(prefix, sample_question, sample_answer, my_ask, temperature, message_thread_context, model, print = True):
+    messages_blank = []
+    messages = update_messages(
+        messages = messages_blank, 
+        system_content=f'{prefix}; Sample question: {sample_question} Sample response: {sample_answer} Preceding conversation: {message_thread_context}', 
+        assistant_content='',
+        user_content=my_ask,
+        )
+
+    api_key = st.secrets["OPENAI_API_KEY"]
+    client = OpenAI(
+        api_key=api_key,
+    )
+    params = {
+        "model": "gpt-3.5-turbo-1106",
         "messages": messages,
         "temperature": temperature,
         "stream": True,
@@ -171,8 +177,8 @@ if "tutor_user_topic" not in st.session_state:
 if "tutor_user_answer" not in st.session_state:
     st.session_state["tutor_user_answer"] = []
     
-if "history" not in st.session_state:
-    st.session_state["history"] = []
+if "message_thread" not in st.session_state:
+    st.session_state["message_thread"] = []
 
 
 if check_password():
@@ -186,38 +192,35 @@ if check_password():
 
 
 
-    name = st.text_input("Please enter your first name only:")
-    if st.button("Let's Learn"):
-        st.session_state.history = ""   
-        initial_question = "Teach me about the cell"
-        docs = st.session_state.vectorstore.similarity_search("Teach me about the cell")
-        full_question = f'Username: {name} My input: {initial_question} /n/n **Respond `Hi {name},` and use the reference {docs} for topics and appropriate level of detail for a high school freshman. **Accuracy is essential** teaching!'
-        with st.spinner("Thinking..."):
-            answer_for_learner = answer_using_prefix(prefix = bio_tutor, sample_question = "", sample_answer = "", my_ask = full_question, temperature = st.session_state.temp, history_context = st.session_state.history, model = st.session_state.model, print = True)
-        st.session_state.history += (f'Question: {initial_question} AI answer: {answer_for_learner}')   
-        st.session_state.tutor_user_topic.append(initial_question)
-        st.session_state.tutor_user_answer.append(answer_for_learner)
-        
-            
-    user_topic = st.text_input("Please enter your topic:")
     
+    # if st.button("Let's Learn"):
+    #     st.session_state.message_thread = ""   
+    #     initial_question = "Teach me about the cell"
+    #     docs = st.session_state.vectorstore.similarity_search("Teach me about the cell")
+    #     full_question = f'Username: {name} My input: {initial_question} /n/n **Respond `Hi {name},` and use the reference {docs} for topics and appropriate level of detail for a high school freshman. **Accuracy is essential** teaching!'
+    #     with st.spinner("Thinking..."):
+    #         answer_for_learner = answer_using_prefix(prefix = bio_tutor, sample_question = "", sample_answer = "", my_ask = full_question, temperature = st.session_state.temp, message_thread_context = st.session_state.message_thread, model = st.session_state.model, print = True)
+    #     st.session_state.message_thread += (f'Question: {initial_question} AI answer: {answer_for_learner}')   
+    #     st.session_state.tutor_user_topic.append(initial_question)
+    #     st.session_state.tutor_user_answer.append(answer_for_learner)
+        
+    model = st.sidebar.selectbox("Select a model", ["gpt-4-1106-preview", "gpt-3.5-turbo-1106", ])
+    
+    name = st.text_input("Please enter your first name:")
+    user_input = st.text_input("Your input here, ask to be tough, ask for test questions, submit your responses, etc.:")    
+    system_context = bio_tutor.format(name = name, outline = biology_outline)
+    if st.session_state.message_thread == []:
+        st.session_state.message_thread = [{"role": "system", "content": system_context}]
         
 
     if st.button("Learn about biology!"):
-        # index_context = f'Use only the reference document for knowledge. Question: {user_topic}'
-        docs = st.session_state.vectorstore.similarity_search(user_topic)
-        user_topic_context = f"User: {name},  with a follow-up question: {user_topic} /n/n Answer focusing on {docs} for topics to explain at a high school level. Address the user using the right name, {name}, in your response, e.g., {name}, .... **Accuracy is essential** for education."
-        # chain = load_qa_chain(llm=llm, chain_type="stuff")
-        # with get_openai_callback() as cb:
-        #     answer_for_learner = chain.run(input_documents = docs, question = chain)
-        # name
+        st.session_state.message_thread.append({"role": "user", "content": user_input})
         with st.spinner("Thinking..."): 
-            answer_for_learner = answer_using_prefix(prefix = bio_tutor, sample_question = "", sample_answer = "", my_ask = user_topic_context, temperature = st.session_state.temp, history_context = st.session_state.history, model = st.session_state.model, print = True)
-        # Append the user question and tutor answer to the session state lists
-        st.session_state.tutor_user_topic.append(f'{name}: {user_topic}')
-        st.session_state.tutor_user_answer.append(answer_for_learner)
-        st.session_state.history.append(f'{name}: {user_topic} AI answer: {answer_for_learner}')
-        st.session_state.history.append({"role": "user", "content": user_topic})
+            answer_for_learner = gen_response(messages = st.session_state.message_thread, temperature = st.session_state.temp, model = model, print = True)
+        st.session_state.tutor_user_topic.append(f'{name}: {user_input}')
+        st.session_state.tutor_user_answer.append(answer_for_learner)        
+        st.session_state.message_thread.append({"role": "assistant", "content": answer_for_learner})
+        
 
         # Display the tutor answer
         # st.write(answer_for_learner)
@@ -231,7 +234,7 @@ if check_password():
 
         # Display the expander section with the full thread of questions and answers
         
-    if st.session_state.history != "":    
+    if st.session_state.message_thread != "":    
         with st.sidebar.expander("Your Conversation", expanded=False):
             for i in range(len(st.session_state.tutor_user_topic)):
                 st.info(f"{st.session_state.tutor_user_topic[i]}", icon="🧐")
@@ -241,4 +244,4 @@ if check_password():
                 st.download_button('Download', st.session_state.current_thread, key='tutor_questions')
     
     if st.sidebar.button("Start a new conversation"):
-        st.session_state.history = []
+        st.session_state.message_thread = []
--- prompts.py
+++ prompts.py
@@ -1,7 +1,102 @@
-bio_tutor="""You are a tutoring a high school freshma, {name} in material topics outlined here {docs} who is anxious about their upcoming exam. 
-You use analogies the student will understand to help. You teach about one or two concepts at a time and invite the user to ask
-more questions if needed. Your content basis for what is covered is included in the context. You skilled at optimal teaching methods
-and can generate mcq questions for the student to answer. You can also generate a quiz for the student to take."""
 
-mcq_prompt="""You are a high school teacher who is giving a multiple choice exam about the {topic} topic. Use the {context} to understand
-what sort of questions to ask. You generate {num_questions} questions. Instruct the user how to respond, e.g, 1. A, 2. B, etc."""
+disclaimer = """**Disclaimer:** This is a tool to teach students"""
+
+bio_tutor="""You are a tutoring a high school freshman, {name} on the material continained in the outline who is anxious about their upcoming exam. 
+You use analogies the student will understand to help. You teach about one or two concepts at a time and invite the user to ask
+more questions if needed. Your content basis for what is covered is included in the context. You are skilled at optimal teaching methods
+and can generate mcq questions for the student to answer. You can also generate a quiz for the student to take. 
+Outline:
+
+{outline}
+"""
+
+biology_outline = """# High School Freshman Biology Study Guide
+
+## Intro/Chemistry of Life:
+
+### Basic Concepts:
+- Element
+- Atom
+- Compound
+- Molecule
+- Nucleus
+- Proton, Neutron, Electron
+- Valence Shell/Electrons
+- Ion, Cation, Anion
+- Ionic Bonds
+- Covalent Bonds
+- Polar Molecule
+- Hydrogen Bond
+
+### Organic Chemistry:
+- Organic
+- Monomer
+- Polymer
+- Carbohydrates
+- Lipids
+- Proteins
+- Nucleic Acids
+
+### Enzymes and Energy:
+- Enzymes
+- Substrate
+- Exo/Endothermic Reactions
+- ATP
+
+## Atomic Structure:
+- Difference between atoms, elements, compounds
+- Structure of an atom and different types (isotopes, ions, etc.)
+- How to use the periodic table to learn about an atom
+
+## Bonding:
+- Definition of bond types
+- Polar molecules
+- Hydrogen bonds
+
+## Water:
+- Four characteristics of water and their importance for life on Earth
+
+## Macromolecules:
+- Monomers and polymers of each type of macromolecule
+- Examples and functions of each
+
+## Enzymes:
+- What chemical reactions are, including terms like reactant, product, substrate
+- Identification and contrast of exothermic and endothermic reactions
+- ATP, where the energy is stored in it, and how it powers cellular work
+
+## Cellular Biology:
+
+### Basic Concepts:
+- Cell
+- Prokaryotic, Eukaryotic
+- Animal Cell, Plant Cell
+
+### Cell Organelles and Structure:
+- Cell Organelles
+- Cell Membrane
+- Selectively Permeable
+- Lipid Bilayer
+- Peripheral and Integral Proteins
+
+### Endomembrane Transport System:
+- Endomembrane Transport System
+- Passive Transport
+- Diffusion
+- Facilitated Diffusion
+- Osmosis
+- Hypertonic, Hypotonic, Isotonic
+- Active Transport: Endocytosis, Exocytosis
+
+## Cells:
+- Different cell types and how they differ
+- Function and structural information of every organelle
+- Identification of Prokaryotic, Eukaryotic, Animal, and Plant cells and any organelles in images
+
+## Cellular Transport:
+- Description and identification of the parts of the cell membrane and their function
+- Differences between active and passive transport
+- Types of active and passive transport
+- Visual identification and description of different osmotic states
+
+This structure should help in understanding the different topics and subtopics in a logical and organized manner. It also allows for easy navigation through the content."""
